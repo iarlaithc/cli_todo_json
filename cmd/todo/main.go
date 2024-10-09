3e667d3cd7c5819aee49b8558166348447ecf695
iarlaithc <57463324+iarlaithc@users.noreply.github.com>
2024-10-10 00:44:59 +0100

    added delete

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -12,8 +12,13 @@ const (
 )
 
 func main() {
+	// flags
 	add := flag.Bool("add",false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark todo as complete")
+	delete := flag.Int("delete", 0, "delete a todo")
 	flag.Parse()
+
+	// init todos 
 	todos := &todo.Todos{}
 
 	if err := todos.Load(todoFile); err != nil {
@@ -25,6 +30,30 @@ func main() {
 	case *add:
 		todos.Add("Sample todo")
 		err := todos.Store(todoFile)
+		if err != nil{
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil{
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil{
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *delete > 0:
+		err := todos.Delete(*delete)
+		if err != nil{
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
 		if err != nil{
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
