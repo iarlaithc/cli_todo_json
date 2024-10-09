0163fc88b400dc9a2b0631e094309bf60f17ae69
iarlaithc <57463324+iarlaithc@users.noreply.github.com>
2024-10-09 23:32:29 +0100

    updates

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	todo "github/.com/iarlaithc/cli-todo-json"
+	"os"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
-	fmt.Println("hello world");
+	add := flag.Bool("add",false, "add a new todo")
+	flag.Parse()
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	} 
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil{
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
